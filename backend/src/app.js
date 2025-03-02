d57f4d533e4acba061d68717160f5964310801a9
Raghulkannan005 <raghulkannan005@gmail.com>
2025-03-03 00:52:59 +0530

    cors update

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,7 +13,10 @@ const app = express();
 dotenv.config();
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: [
+        "http://localhost:5173",
+        "https://zorp-hotel.vercel.app"
+    ]
 }));
 
 // Middleware
